fef36f881e156992fbbb90093fcc7c89914dd3f2
Maksym Zhmura <zmuramaksim@gmail.com>
2026-05-13 13:17:23 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Any
+from typing import Any, Self
 
 
 class CleanUpFile:
@@ -7,8 +7,8 @@ class CleanUpFile:
     def __init__(self, filename: str) -> None:
         self.filename = filename
 
-    def __enter__(self) -> None:
-        return None
+    def __enter__(self) -> Self:
+        return self
 
     def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
         if os.path.exists(self.filename):
